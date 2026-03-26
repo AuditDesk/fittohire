4d31e467555fafc52c4d442e17c1e34d5d722ffe
AuditDesk <auditdesk.hq@gmail.com>
2026-03-26 17:04:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from fastapi.templating import Jinja2Templates
 import os
 
 app = FastAPI()
 
-# Static files (CSS, JS, images)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Register routers
-from app.routers import auth, payments
+from app.routers import auth, payments, dashboard
 app.include_router(auth.router)
 app.include_router(payments.router)
+app.include_router(dashboard.router)
 
-# Existing pages
 @app.get("/")
 async def home():
     return FileResponse("static/index.html")
